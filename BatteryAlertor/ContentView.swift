f6980015d66f63a90f686744758a2e1309eb5f05
DingChil <xu.dingchao@gmail.com>
2020-04-16 13:29:40 +0800

    First time to upload the project.

---
+++ BatteryAlertor/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  BatteryAlertor
+//
+//  Created by Chil on 16/4/20.
+//  Copyright © 2020 Chil. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Yep, There is Battery Alertor.")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
